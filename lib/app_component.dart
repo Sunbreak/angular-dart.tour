1b3b7d5bb5bc4ef574db3b89252e3f7ef8209df0
Sunbreak <sunbreak.wang@gmail.com>
2020-08-10 18:31:19 +0800

    Services
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,18 +2,30 @@ import 'package:angular/angular.dart';
 
 import 'hero_component.dart';
 import 'src/hero.dart';
-import 'src/mock_heroes.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
+  providers: [ClassProvider(HeroService)],
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
   directives: [coreDirectives, HeroComponent],
 )
-class AppComponent {
+class AppComponent implements OnInit {
+  final HeroService _heroService;
+
+  AppComponent(this._heroService);
+
   final title = 'Tour of Heroes';
 
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAllSlowly();
+  }
+
+  @override
+  void ngOnInit() => _getHeroes();
 
   Hero selected;
 
